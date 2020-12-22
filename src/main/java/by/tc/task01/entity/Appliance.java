2e63db48fa51567571225eb345c7f8b7ab65a4cb
AleksandrAnashkevich <alexlida98@gmail.com>
2020-12-22 15:29:42 +0300

    ///

---
+++ src/main/java/by/tc/task01/entity/Appliance.java
@@ -0,0 +1,16 @@
+package by.tc.task01.entity;
+
+public class Appliance {
+    private String name;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // you may add your own code here
+}
